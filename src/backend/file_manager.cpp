6146335ce4b07fcb26181fc8ff24de6e0c4fa7ad
pushkar-gr <gr.pushkar.7@gmail.com>
2024-12-02 13:46:03 +0530

    add: few basic functions

--- src/backend/file_manager.cpp
+++ src/backend/file_manager.cpp
@@ -1,1 +1,121 @@
 #include "file_manager.h"
+#include <cstdio>
+#include <filesystem>
+#include <vector>
+
+using namespace std::filesystem;
+using namespace std;
+
+FileManager::FileManager() {
+  FileManager(path("."));
+}
+
+FileManager::FileManager(path path) {
+  switchPath(path);
+}
+
+path FileManager::getCurrentPath() {
+  return currentPath;
+}
+
+path FileManager::getSelectedPath() {
+  return selectedPath;
+}
+
+bool FileManager::selectPath(path path, bool skipCheck=false) {
+  if (! skipCheck) {
+    for (int i = 0; i < children.size(); i++) {
+      if (children[i] == path) {
+        return selectPath(i);
+      }
+    }
+  }
+  if (path == selectedPath || path.empty()) {
+    return false;
+  }
+  selectedPath = path;
+  selected = -1;
+  return true;
+}
+
+bool FileManager::selectPath() {
+  if (selected < 0 || selected >= children.size()) {
+    return false;
+  }
+  selectedPath = children[selected].path();
+  return true;
+}
+
+bool FileManager::selectPath(int index) {
+  if (selected == -1) {
+    selected = 0;
+  }
+  int oldIndex = selected;
+  selected = index;
+  if (! selectPath()) {
+    selected = oldIndex;
+    return false;
+  }
+  return true;
+}
+
+bool FileManager::incrementSelected(bool cycle=true) {
+  if (selected == -1) {
+    selected = 0;
+  }
+  if (selected + 1 >= children.size()) {
+    return selectPath(0);
+  }
+  return selectPath(selected + 1);
+}
+
+bool FileManager::decrementSelected(bool cycle=true) {
+  if (selected == -1) {
+    selected = 0;
+  }
+  if (selected == 0) {
+    if (children.size() == 0) {
+      return false;
+    }
+    return selectPath(children.size() - 1);
+  }
+  return selectPath(selected - 1);
+}
+
+bool FileManager::selectParentDir() {
+  if (selected == -1) {
+    return selectedPath == currentPath.parent_path();
+  }
+  return selectPath(currentPath.parent_path(), true);
+}
+
+path FileManager::switchPath(path path) {
+  if (selectPath(path, true)) {
+    return switchPath();
+  }
+  return currentPath;
+}
+
+path FileManager::switchPath() {
+  currentPath = selectedPath;
+  children.clear();
+  int selectedIndex = -1;
+  for (const auto& entry : directory_iterator(currentPath)) {
+    children.push_back(entry);
+    if (entry.path() == selectedPath) {
+      selectedIndex = children.size() - 1;
+    }
+  }
+
+  selectedPath = NULL;
+  selected = selectedIndex == -1 ? 0 : selectedIndex;
+  selectPath();
+  return currentPath;
+}
+
+path FileManager::switchToParent() {
+  if (selectParentDir()) {
+    return switchPath();
+  }
+  return currentPath;
+}
--- src/backend/file_manager.h
+++ src/backend/file_manager.h
@@ -1,5 +1,35 @@
 #ifndef FILE_MANAGER_H
 #define FILE_MANAGER_H
+#include <filesystem>
+#include <vector>
+
+class FileManager {
+private:
+  std::filesystem::path currentPath;
+  std::vector<std::filesystem::directory_entry> children;
+
+  std::filesystem::path selectedPath;
+  int selected;
+
+public:
+  FileManager(); //constructor, initializes at current directory
+  FileManager(std::filesystem::path); //constructor, initializes at given directory
+
+  std::filesystem::path getCurrentPath(); //gets the current directory program is in
+
+  std::filesystem::path getSelectedPath(); //gets the selected path by user
+  bool selectPath(std::filesystem::path, bool); //selelcts the given file
+  bool selectPath(); //updates the selected file (selectedPath) based on index (selected)
+  bool selectPath(int); //updates the index (selected) and updates the selected file (selectedPath)
+  bool incrementSelected(bool); //selects next file in the current directory
+  bool decrementSelected(bool); //selects previous file in the current directory
+  bool selectParentDir(); //selects the parent directory
+
+  std::filesystem::path switchPath(std::filesystem::path); //switchs to given directory
+  std::filesystem::path switchPath(); //switches to selected directory (selectedPath)
+  std::filesystem::path switchToParent(); //switches to parent directory
+
+  ~FileManager(); //destructor, does nothing as of now
+};
 
 #endif
-
