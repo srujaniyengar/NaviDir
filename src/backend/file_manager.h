13e708a2bfe450b188c5238fc24041abdba92ae3
pushkar-gr <gr.pushkar.7@gmail.com>
2024-12-04 22:39:44 +0530

    update: used namespaces

--- src/backend/file_manager.h
+++ src/backend/file_manager.h
@@ -5,43 +5,46 @@
 #include <vector>
 #include <string>
 
+using namespace std::filesystem;
+using namespace std;
+
 class FileManager {
 private:
-  std::filesystem::path currentPath;
-  std::vector<std::filesystem::directory_entry> children;
+  path currentPath;
+  vector<directory_entry> children;
 
-  std::filesystem::path selectedPath;
+  path selectedPath;
   int selected;
 
-  std::variant<std::vector<std::filesystem::directory_entry>, std::string> selectedData;
+  variant<vector<directory_entry>, string> selectedData;
 
-  void updateChildren(std::vector<std::filesystem::directory_entry>*, std::filesystem::path path);
+  void updateChildren(vector<directory_entry>*, path path);
 
   void updateSelectedData();
 
 public:
   FileManager(); //constructor, initializes at current directory
-  FileManager(std::filesystem::path); //constructor, initializes at given directory
+  FileManager(path); //constructor, initializes at given directory
 
-  std::filesystem::path getCurrentPath(); //gets the current directory program is in
+  path getCurrentPath(); //gets the current directory program is in
 
-  std::vector<std::filesystem::directory_entry> getChildren(); //gets children of current directory
+  vector<directory_entry> getChildren(); //gets children of current directory
   
-  std::filesystem::directory_entry getSelectedItem(); //gets selected item
+  directory_entry getSelectedItem(); //gets selected item
 
-  std::filesystem::path getSelectedPath(); //gets the selected path by user
-  bool selectPath(std::filesystem::path, bool); //selelcts the given file
+  path getSelectedPath(); //gets the selected path by user
+  bool selectPath(path, bool); //selelcts the given file
   bool selectPath(); //updates the selected file (selectedPath) based on index (selected)
   bool selectPath(int); //updates the index (selected) and updates the selected file (selectedPath)
   bool incrementSelected(bool); //selects next file in the current directory
   bool decrementSelected(bool); //selects previous file in the current directory
   bool selectParentDir(); //selects the parent directory
   
-  std::variant<std::vector<std::filesystem::directory_entry>, std::string> getSelectedData();
+  variant<vector<directory_entry>, string> getSelectedData();
 
-  std::filesystem::path switchPath(std::filesystem::path); //switchs to given directory
-  std::filesystem::path switchPath(); //switches to selected directory (selectedPath)
-  std::filesystem::path switchToParent(); //switches to parent directory
+  path switchPath(path); //switchs to given directory
+  path switchPath(); //switches to selected directory (selectedPath)
+  path switchToParent(); //switches to parent directory
 
   ~FileManager(); //destructor, does nothing as of now
 };
