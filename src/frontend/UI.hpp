66cb398a70bdfc32bb6a6ec17ae15abb61967e7e
srujaniyengar <srujanparthasarathyiyengar@gmail.com>
2025-01-05 16:27:30 +0530

    chore:populated UI.hpp

--- src/frontend/UI.hpp
+++ src/frontend/UI.hpp
@@ -1,4 +1,24 @@
-#ifndef UI_HPP
-#define UI_HPP
 
-#endif
+#pragma once
+#include "ftxui/component/captured_mouse.hpp"
+#include "ftxui/component/component.hpp"
+#include "ftxui/component/component_base.hpp"
+#include "ftxui/component/screen_interactive.hpp"
+#include "ftxui/dom/elements.hpp"
+
+class UI {
+public:
+    UI();
+    void run();
+
+private:
+    std::vector<std::string> tab_values;
+    int tab_selected;
+    std::vector<std::string> tab_1_entries;
+    std::vector<std::string> tab_2_entries;
+    std::vector<std::string> tab_3_entries;
+    int tab_1_selected;
+    int tab_2_selected;
+    int tab_3_selected;
+};
+
